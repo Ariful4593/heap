fc9a0d839cb6643751596971d355f11d8c7bc238
Ariful4593 <arifkhagoria2015@gmail.com>
2021-02-21 12:29:47 +0600

    Heap sort code added

--- heap_sort.c
+++ heap_sort.c
@@ -44,6 +44,21 @@ void build_max_heap(int heap[], int heap_size)
     {
         max_heapify(heap, heap_size, i);
     }
+
+}
+
+void heap_sort(int heap[], int heap_size)
+{
+    int i,t;
+
+    for(i = heap_size; i > 1; i--)
+    {
+        t = heap[1];
+        heap[1] = heap[i];
+        heap[i] = t;
+        heap_size -= 1;
+        max_heapify(heap, heap_size, 1);
+    }
 }
 
 void print_heap(int heap[], int heap_size)
@@ -57,17 +72,14 @@ void print_heap(int heap[], int heap_size)
 
 int main()
 {
-    int heap_size = 9;
-    int heap[] = {0,12,7,1,3,10,17,19,2,5};
-
-    print_heap(heap, heap_size);
-    printf("\n");
-
-    max_heapify(heap, heap_size, 3);
-    print_heap(heap, heap_size);
-    printf("\n");
-
+    int heap_size = 8;
+    //int heap[] = {0,12,7,1,3,10,17,19,2,5};
+    int heap[] = {0, 48,35,52,25,55,70,63,58};
     build_max_heap(heap, heap_size);
+    print_heap(heap, heap_size);
+    printf("\n");
+
+    heap_sort(heap, heap_size);
     print_heap(heap, heap_size);
 
     return 0;
